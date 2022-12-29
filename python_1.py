535b490d8387f5bf49916295b2a0b9bd30f13582
mahmud10cool <mutashi@gmail.com>
2022-12-29 10:13:09 +0600

    First file changed

--- python_1.py
+++ python_1.py
@@ -1,1 +1,28 @@
-print("Hello world")
+# We separate words using underscore 
+# Make variable names that make sense
+# Put double quotes if you want to put single quote in the string
+# Or you can put a backslash \ to identify the single quote 
+my_message = "Bobby's world"
+
+# Put three quotations marks for multiple line strings
+multi_line_message = """This is a multi line string. 
+So please understand"""
+
+# We can find the length of the message using the len() function
+length_message = len(my_message)
+
+print(my_message)
+print(multi_line_message)
+print(length_message)
+
+# Indexing
+# You can use my_message[position - 1] to find the character in that position
+# Example:
+character_1 = my_message[1-1]
+character_2 = my_message[2-1]
+# and so on and so forth
+# I will print to demonstrate
+print(character_1)
+print(character_2)
+
+
