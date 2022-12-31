e22113fb18edaaefaabec8080de31790f8669510
mahmud10cool <mutashi@gmail.com>
2022-12-31 18:13:41 +0600

    Revised lecture 1 and lecture 2. Lecture 3 left.

--- revision_1.py
+++ revision_1.py
@@ -1,4 +1,7 @@
-# First let us start with strings
+## Revision 1
+# I will revise the first three lectures here
+
+## Strings
 name = 'Mahmud Suhaimi Ibrahim'
 university = 'IIUM'
 gender = 'non-conforming'
@@ -39,4 +42,66 @@ I study at {university}"""
 # Print to show example
 print(overall_string)
 
+## Integers and floats
+# Of course when comes to integers and floats, one of the most 
+# important thing is operators
+# First define two integers
+x = 3
+y = 2
+# Two floats as well
+p = 5.3
+q = 2.7
 
+# Addition
+print(x + y)
+print(p + q)
+print(x + p) # This will return a float
+
+# Subtraction
+print(y - x)
+print(p - q)
+
+# Multiplication
+print(p*x*q)
+
+# Division
+print(q/x)
+
+# Exponent
+print(q**p)
+
+# Modulus
+# This is used to check if a number is even or odd
+# Also can be used to check if a number is 0 or 1
+print(x%y)
+
+# BODMAS or rules of operations definitely apply here
+print(y - x / x + y)
+print((y - x)/(x + y))
+
+# Division has a tendency of returning a float
+# If you want division to return an integer, there are two 
+# functions that you can use
+# int() function
+# This floors the value
+print(int(y/x))
+# round() function
+# This rounds up
+print(round(y/x))
+# You can also use the round function to round to a certain 
+# decimal place
+print(round(y/x, 2))
+
+# Comparators
+# These will compare two numbers
+# Using these will result in a boolean value of True or False
+# A few examples
+print(y > x)
+print(y != x)
+
+# Other use of the float and int functions
+# In Python, we can use these functions to conver 'number' strings
+# into integers and floats
+x = '123'
+print(int(x))
+print(float(x))
