04a9899e64919a31cc95eb00c569711c0dfd92cf
mahmud10cool <mutashi@gmail.com>
2022-12-31 16:33:07 +0600

    Third python lecture

---
+++ python_3.py
@@ -0,0 +1,13 @@
+# This lecutre is about lists, tuples and sets
+courses = ['History', 'Maths', 'Physics', 'CompSci']
+length_list = len(courses)
+# We can use indexing here as well
+first_item = courses[0]
+print(first_item)
+# We can access two of the values in the list
+# Assign the index to a variable 'n' for better understanding
+n = 2
+# The code below will only select the items until 'n-1'
+range_items = courses[0:n]
+print(range_items)
+
