62a12c827d6be4439936e5e2598c00c3cb3ab00a
mahmud10cool <mutashi@gmail.com>
2023-01-05 16:46:00 +0600

    More things

--- python_4.py
+++ python_4.py
@@ -1,1 +1,13 @@
 ## Lecture 4
+
+# Dictionaries
+# We have two important parameters
+# Key and a value
+# We follow the key with a semicolon and then put the value
+# Kind of like the word followed by the meaning
+student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
+# If we want to print the value (meaning) behind any of the keys (words)
+# Then we just print the dictionary with the key being the index
+print(student['name'])
+print(student['courses'])
+
