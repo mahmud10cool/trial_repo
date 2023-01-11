833d774e49cae967f08c06c534a4d931c325d941
mahmud10cool <mutashi@gmail.com>
2023-01-12 00:25:04 +0600

    Understood the scope of variables

--- python_extra_1.py
+++ python_extra_1.py
@@ -4,11 +4,23 @@ LEGB
 Local, Enclosing, Global, Built-in
 '''
 
+# Importing the builtin modules to see the built-in variables
+import builtins
+
+# Checking all the builtin variables
+# print(dir(builtins))
+# print()
+
+# Note: Python gives us access to overwrite all these builtin variables and functions
+
+m = min([5, 1, 4, 2, 3])
+print(m)
+
 # A global variable
 x = 'global x'
 
 # A local variable
-def test():
+def test(z):
     # Put global x in the function
     # If I change the value of x from within the function now
     # The value of x will change globally because of the command below
@@ -18,12 +30,36 @@ def test():
     print(y)
     # Printing the global variable from within a function
     print(x)
+    # Using the local variable z
+    print(z)
 
-test()
+test('local z')
+
+print()
 
 # Printing the global variable from outside the function
 # To show that it is a global variable
 print(x)
 
+print()
+
+# Nested functions
+# We can understand the enclosing scope using this
+def outer():
+    x = 'outer x'
+
+    def inner():
+        # The commented code below will affect the outer x, the one in the enclosing function
+        # nonlocal x
+        # If there is a local x to the inner function, the function will use that
+        # If it doesn't then it will use the outer x
+        # It does not have that either, then it will use the global value
+        x = 'inner x'
+        print(x)
+
+    inner()
+    print(x)
+
+outer()
 
 
