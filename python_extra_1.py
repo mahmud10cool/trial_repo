7382002eb8ac693be32cd9d39cf3dc0f854d0a9b
mahmud10cool <mutashi@gmail.com>
2023-01-11 22:40:50 +0600

    Small changes made

--- python_extra_1.py
+++ python_extra_1.py
@@ -9,12 +9,21 @@ x = 'global x'
 
 # A local variable
 def test():
+    # Put global x in the function
+    # If I change the value of x from within the function now
+    # The value of x will change globally because of the command below
+    global x
+    # Make a local y
     y = 'local y'
     print(y)
     # Printing the global variable from within a function
     print(x)
 
 test()
+
+# Printing the global variable from outside the function
+# To show that it is a global variable
+print(x)
 
 
 
